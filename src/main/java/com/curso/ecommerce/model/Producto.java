dc712a99a3e50dbfdaa3f266e512ec806eea164e
Mike Sarmiento <miguel.sarmiento@i2cat.net>
2023-10-13 14:45:26 +0200

    Creacion de la estructura del proyecto y de la clases model

---
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -0,0 +1,82 @@
+package com.curso.ecommerce.model;
+
+public class Producto {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
+    private Double precio;
+    private int cantidad;
+
+    public Producto() {
+    }
+
+    public Producto(Integer id, String nombre, String descripcion, String imagen, Double precio, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", imagen='" + imagen + '\'' +
+                ", precio=" + precio +
+                ", cantidad=" + cantidad +
+                '}';
+    }
+}
---
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -0,0 +1,104 @@
+package com.curso.ecommerce.model;
+
+public class Usuario {
+    private Integer id;
+    private String nombre;
+    private String username;
+    private String email;
+    private String dirrecion;
+    private String telefono;
+    private String tipo;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String nombre, String username, String email, String dirrecion, String telefono, String tipo, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.username = username;
+        this.email = email;
+        this.dirrecion = dirrecion;
+        this.telefono = telefono;
+        this.tipo = tipo;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDirrecion() {
+        return dirrecion;
+    }
+
+    public void setDirrecion(String dirrecion) {
+        this.dirrecion = dirrecion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", dirrecion='" + dirrecion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
