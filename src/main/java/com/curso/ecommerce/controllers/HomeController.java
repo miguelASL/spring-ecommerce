f9d9d80be9acb3f2b69c7d38aeb5afd48fa740d4
Mike Sarmiento <miguel.sarmiento@i2cat.net>
2023-10-14 18:17:09 +0200

    Obtener la cantidad y buscar el porducto en el home controller y añadirlo

--- src/main/java/com/curso/ecommerce/controllers/HomeController.java
+++ src/main/java/com/curso/ecommerce/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.controllers;
 
+import com.curso.ecommerce.model.DetalleOrden;
+import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.service.ProductoService;
 import org.slf4j.Logger;
@@ -7,10 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -21,6 +23,11 @@ public class HomeController {
 
     @Autowired
     private ProductoService productoService;
+    // Para almacenar los productos que se agregan al carrito
+    List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+    // Datos de la orden
+    Orden orden = new Orden();
     @GetMapping("/")
     public String home(Model model){
         model.addAttribute("productos", productoService.findAll());
@@ -34,5 +41,18 @@ public class HomeController {
         producto = productoOptional.get();
         model.addAttribute("producto", producto);
         return "usuario/productohome";
+    }
+
+    @PostMapping("/car")
+    public String carrito(@RequestParam Integer id, @RequestParam Integer cantidad, Model model){
+        DetalleOrden detalle = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+
+        Optional<Producto> productoOptional = ProductoService.get(id);
+        assert Objects.requireNonNull(productoOptional).isPresent();
+        log.info("Mostrando producto con id: {}", productoOptional.get());
+        log.info("Cantidad: {}", cantidad);
+        return "usuario/carrito";
     }
 }
